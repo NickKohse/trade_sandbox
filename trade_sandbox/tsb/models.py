ceb57a615e512aa8468c9fb837a29fae04d81f05
Nick Kohse <nick.kohse@gmail.com>
2019-02-21 19:58:44 -0700

    add models

--- trade_sandbox/tsb/models.py
+++ trade_sandbox/tsb/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+class Trade(models.Model):
+    account = models.ForeignKey(Account, on_delete = models.CASCADE)
+    timestamp = models.PositiveIntegerField()
+    num_shares = models.PositiveIntegerField()
+    book_value = models.DecimalField(decimal_places=3, max_digits=8)
+    ticker = models.CharField(max_length = 10)
+
+class Asset(models.Model):
+    account = models.ForeignKey(Account, on_delete = models.CASCADE)
+
+
