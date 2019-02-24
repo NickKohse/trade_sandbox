760ddc5715aeedc8423c7fd6a38e8f4705ee9209
Nick Kohse <nick.kohse@gmail.com>
2019-02-23 19:58:41 -0700

    Add str methods

--- trade_sandbox/tsb/models.py
+++ trade_sandbox/tsb/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return "User: {self.name}"
+
 class Account(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return "Account: {self.id}" 
 
 class Trade(models.Model):
     account = models.ForeignKey(Account, on_delete = models.CASCADE)
@@ -15,7 +21,14 @@ class Trade(models.Model):
     book_value = models.DecimalField(decimal_places=3, max_digits=8)
     ticker = models.CharField(max_length = 10)
 
+    def __str__(self):
+        return "Trade: {self.id} - {self.ticker} - {self.timestamp}"
+
 class Asset(models.Model):
     account = models.ForeignKey(Account, on_delete = models.CASCADE)
+    trade = models.ForeignKey(Trade, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return "Asset: {self.id}"
 
 
